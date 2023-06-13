716cc81b713f3747e8bead62c53288bc9f246f3a
mrx11f <masanovrauan@gmail.com>
2023-06-13 17:38:00 +0600

    first commit

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+class BlogTestCase(TestCase):
+    def setUp(self) -> None:
+        self.client = Client()
+
+    def test_contacts(self):
+        response = self.client.get(reverse("contacts"))
+        exp_data = "Контактная информация"
+        self.assertEqual(exp_data, response.content.decode())
+
+    def test_contacts(self):
+        response = self.client.get(reverse("about"))
+        exp_data = "О нас"
+        self.assertEqual(exp_data, response.content.decode())
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
 
-# Create your views here.
+
+def get_contacts():
+    return "Контактная информация"
+
+
+def get_about():
+    return "О нас"
+
+
+class ContactsView(View):
+    def get(self, request):
+        contacts = get_contacts()
+        return HttpResponse(contacts)
+
+
+class AboutView(View):
+    def get(self, request):
+        about = get_about()
+        return HttpResponse(about)
+
